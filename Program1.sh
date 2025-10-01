48fa788fec37280bc59f1e8de5ccf97119a9ccb6
chriz-tina <christinaclarissiby2027@cs.sjcetpalai.ac.in>
2025-10-02 01:02:44 +0530

    Create Program1.sh

---
+++ Program1.sh
@@ -0,0 +1,15 @@
+echo "Enter your Name"
+read name
+echo "Enter your semester"
+read semester
+echo "Enter tour Batch"
+read batch
+echo "Enter your Roll Number"
+read rollNum
+echo "Enter your Register Number"
+read regNum
+echo "Name : "$name
+echo "Semester : "$semester
+echo "Batch : "$batch
+echo "RollNumber : "$rollNum
+echo "Register Number: "$regNum
